1107b6bf2709cfae18c72f0641a73a548a595c15
wtholmes <wth1@cornell.edu>
2022-08-07 13:18:01 -0400

    8/7/2022 Minor update to Request O365 TDX Service

--- TDXManager/RequestOffice365AccountTDXService.cs
+++ TDXManager/RequestOffice365AccountTDXService.cs
@@ -14,22 +14,21 @@ namespace TDXManager
     {
         public RequestOffice365AccountTDXService()
         {
+            // Start a ProvAccounts Manager.
+            ProvAccountsManager provAccountsManager = new ProvAccountsManager();
+
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            //TDXAutomationTickets = new List<TDXAutomationTicket>();
-
-            // Inactive Ticket Statuses
+            // ------
+            // Get the list of tickets from TDX using Email and Calendar / Request Office 365 Faculty A3 License report. This report
+            // returns all of the tickets that are using the: Email and Calendar / Request Office 365 Faculty A3 License form. Filter
+            // the report to only return requests that are in an active state by excluding inactive states.
+            // ------
             Regex InactiveTicketsRegex = new Regex(@"(Reopened|Resolved|Closed|Canceled)", RegexOptions.IgnoreCase);
+            GetTicketsUsingReport("* Email and Calendar / Request Office 365 Faculty A3 License", InactiveTicketsRegex);
 
-            // ------
-            // Get the list of tickets from TDX using the Automated E-List Owner Transfer Requests report.
-            // This report returns all of the tickets that are using the:
-            // Discussion and Announcement Email List / Transfer e-List Ownership (V2) TDX Form.
-            // ------
-            GetTicketsUsingReport("* Office 365 / Faculty A3 License Requests", InactiveTicketsRegex);
-
-            // Populate ListOwnerTransferTickets ;
+            // Process the tickets returned from the report.
             foreach (Ticket ticket in this.TDXTickets)
             {
                 if (ticket != null)
@@ -37,61 +36,74 @@ namespace TDXManager
                     String ticketStatus = ticket.StatusName;
                     if (!InactiveTicketsRegex.IsMatch(ticketStatus))
                     {
+                        // Set the Active Ticket, this sets the scope for all fuctions and methods.
                         this.SetActiveTicket(ticket);
 
-                        // Get Automation Status [TDX Custom Attribute: (S111-AUTOMATIONDETAILS)]
+                        // Get Automation Status Details [TDX Custom Attribute: (S111-AUTOMATIONDETAILS)] in
+                        // a StringBuilde so that we can update the automation details to the TDX Request.
                         StringBuilder AutomationDetails = new StringBuilder(this.TDXAutomationTicket.AutomationDetails);
 
-                        // Get Automation ID [TDX Custom Attribute: (S111-AUTOMATIONID)]
-                        if (this.TDXAutomationTicket.AutomationID == null)
-                        {
-                            this.UpdateAttribute("S111-AUTOMATIONID", Guid.NewGuid().ToString());
-                            AutomationDetails.AppendFormat("[{0}]: Automation Status ID has been set.<br />", DateTime.UtcNow.ToString("yyyy:mm:dd:HH:mm"));
-                        }
+                        // Ticket Comments StringBuilder.
+                        StringBuilder TicketComments = new StringBuilder();
 
-                        // Get Automation Status [TDX Custom Attribute: (S111-AUTOMATIONSTATUS)]
+                        // ------
+                        // The Automation Status Attribute [TDX Custom Attribute: (S111-AUTOMATIONSTATUS)] is used to direct
+                        // automation processing. It is intended that it be updated by this class and by TeamDynamix Workflows.
+                        // The standard configuration of TDX forms should not allow for manual updates to (S111-AUTOMATIONSTATUS) unless
+                        // every possible state change can be handled by this class or its parent(s). As with allowing manual updates, when
+                        // creating TeamDynamix workflow consideration must be given to setting (S111-AUTOMATIONSTATUS) such that the follow
+                        // processing steps will run in the desired order or that the processing steps are order independent.
+                        // ------
+
                         switch (this.TDXAutomationTicket.AutomationStatus)
                         {
                             case null:
                                 {
-                                    this.UpdateDropDownChoiceAttribute("S111-AUTOMATIONSTATUS", AUTOMATIONSTATUS.NEW);
+                                    // Update the Automation Status and Automation Status Details.
+                                    this.UpdateAutomationStatus(AUTOMATIONSTATUS.NEW);
                                     AutomationDetails.AppendFormat("[{0}]: AutomationStatus has been set to NEW.   ", DateTime.UtcNow.ToString());
                                     break;
                                 }
                             case var value when value == AUTOMATIONSTATUS.NEW:
                                 {
-                                    this.UpdateDropDownChoiceAttribute("S111-AUTOMATIONSTATUS", AUTOMATIONSTATUS.NEW);
+                                    // Update the Automation Status and Automation Status Details.
+                                    this.UpdateAutomationStatus(AUTOMATIONSTATUS.NEW);
                                     AutomationDetails.AppendFormat("[{0}]: AutomationStatus is set to NEW.   ", DateTime.UtcNow.ToString());
                                     break;
                                 }
 
                             case var value when value == AUTOMATIONSTATUS.INPROCESS:
                                 {
-                                    this.UpdateDropDownChoiceAttribute("S111-AUTOMATIONSTATUS", AUTOMATIONSTATUS.INPROCESS);
+                                    // Update the Automation Status and Automation Status Details.
+                                    this.UpdateAutomationStatus(AUTOMATIONSTATUS.INPROCESS);
                                     AutomationDetails.AppendFormat("[{0}]: AutomationStatus is set to INPROCESS.   ", DateTime.UtcNow.ToString());
                                     break;
                                 }
                             case var value when value == AUTOMATIONSTATUS.PENDINGAPPROVAL:
                                 {
-                                    this.UpdateDropDownChoiceAttribute("S111-AUTOMATIONSTATUS", AUTOMATIONSTATUS.PENDINGAPPROVAL);
+                                    // Update the Automation Status and Automation Status Details.
+                                    this.UpdateAutomationStatus(AUTOMATIONSTATUS.PENDINGAPPROVAL);
                                     AutomationDetails.AppendFormat("[{0}]: AutomationStatus is set to PENDINGAPPROVAL.   ", DateTime.UtcNow.ToString());
                                     break;
                                 }
                             case var value when value == AUTOMATIONSTATUS.COMPLETE:
                                 {
-                                    this.UpdateDropDownChoiceAttribute("S111-AUTOMATIONSTATUS", AUTOMATIONSTATUS.COMPLETE);
+                                    // Update the Automation Status and Automation Status Details.
+                                    this.UpdateAutomationStatus(AUTOMATIONSTATUS.COMPLETE);
                                     AutomationDetails.AppendFormat("[{0}]: AutomationStatus is set to COMPLETE.   ", DateTime.UtcNow.ToString());
                                     break;
                                 }
                             case var value when value == AUTOMATIONSTATUS.CANCELED:
                                 {
-                                    this.UpdateDropDownChoiceAttribute("S111-AUTOMATIONSTATUS", AUTOMATIONSTATUS.CANCELED);
+                                    // Update the Automation Status and Automation Status Details.
+                                    this.UpdateAutomationStatus(AUTOMATIONSTATUS.CANCELED);
                                     AutomationDetails.AppendFormat("[{0}]: AutomationStatus is set to CANCELED.   ", DateTime.UtcNow.ToString());
                                     break;
                                 }
                             case var value when value == AUTOMATIONSTATUS.DECLINED:
                                 {
-                                    this.UpdateDropDownChoiceAttribute("S111-AUTOMATIONSTATUS", AUTOMATIONSTATUS.DECLINED);
+                                   // Update the Automation Status and Automation Status Details.
+                                    this.UpdateAutomationStatus(AUTOMATIONSTATUS.DECLINED);
                                     AutomationDetails.AppendFormat("[{0}]: AutomationStatus is set to DECLINED.   ", DateTime.UtcNow.ToString());
                                     break;
                                 }
